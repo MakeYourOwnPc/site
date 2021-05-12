632c72f50dee8f33390fd52dc26b96d9aeb13eb0
EmmanuelEngineer <e.coppola37@studenti.unisa.it>
2021-05-12 17:13:02 +0200

    lib

--- src/main/java/Model/Mobo/MoboDao.java
+++ src/main/java/Model/Mobo/MoboDao.java
@@ -10,7 +10,7 @@ public interface MoboDao <E extends Exception>{
     Mobo doRetrieveByName(String name)throws E;
     ArrayList<Mobo> doRetrieveByCpuSocket(String socket) throws E;
     ArrayList<Mobo> doRetrieveByRamSocket(String socket) throws E;
-    ArrayList<Mobo> doRetrieveByFormFactor(String FormFactor) throws E;
+    ArrayList<Mobo> doRetrieveByFormFactor(String formFactor) throws E;
     ArrayList<Mobo> doRetrieveByAmountOfSlotNvme(int slot) throws E;
     ArrayList<Mobo> doRetrieveByAmountOfSlotSata(int slot) throws E;
     boolean doDeleteBySn(String sn) throws E;
