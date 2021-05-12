eff11844cb25e860d847f628cd006e2e975161e8
alemarigliano16 <a.marigliano16@studenti.unisa.it>
2021-05-12 18:35:52 +0200

    pippo

---
+++ src/main/java/Model/Country/CountryDao.java
@@ -0,0 +1,7 @@
+package Model.Country;
+
+import java.util.ArrayList;
+
+public interface CountryDao {
+    ArrayList<Country> doRetrieveAll();
+}
--- src/main/java/Model/User/UserDao.java
+++ src/main/java/Model/User/UserDao.java
@@ -6,7 +6,6 @@ public interface UserDao <E extends Exception>{
     ArrayList<User> doRetrieveAll()throws E;
     User doRetrieveByEmail(String email)throws E;
     boolean doSave(User user)throws E;
-
     boolean doDelete(User user)throws E;
     boolean doUpdate(User user)throws E;
     boolean doSetAdminByEmail(String email)throws E;
