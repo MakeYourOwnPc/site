63554c9aeb3446f339dbd55b1c6a7b0eb1d9b7eb
EmmanuelEngineer <e.coppola37@studenti.unisa.it>
2021-07-07 23:09:50 +0200

    front-end

--- src/main/java/Controller/adminPage.java
+++ src/main/java/Controller/adminPage.java
@@ -1,4 +1,4 @@
-package MakeYourOwnPc.MYOPSite;
+package Controller;
 
 import Model.User.User;
 
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-@WebServlet(name="admin",urlPatterns = "/admin")
 public class adminPage extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
