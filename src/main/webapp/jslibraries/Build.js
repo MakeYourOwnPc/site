6a9f7e2c143224f3a6e80a17aff6a55d265e64af
EmmanuelEngineer <e.coppola37@studenti.unisa.it>
2021-07-07 12:59:04 +0200

    front-end

--- src/main/webapp/jslibraries/Build.js
+++ src/main/webapp/jslibraries/Build.js
@@ -1,27 +1,28 @@
-let mobo;
-let gpu;
-let cpu;
-let ram;
-let massStorage1;
-let massStorage2;
-let massStorage3;
-let psu;
-let pcCase;
-let arrayElements;
-let formFactor;
-let slotSataAvailable;
-let slotNVMEAvailable;
-let slotRamAvailable;
-let slotRamUsed;
-let slotSataUsed;
-let slotNVMEUsed;
-let powerNeeded=0;
-let socketRam;
-let socketCpu;
+let mobo = null;
+let gpu = null;
+let cpu = null;
+let ram = null;
+let massStorage1 = null;
+let massStorage2 = null;
+let massStorage3 = null;
+let psu = null;
+let pcCase = null;
+let arrayElements = null;
+let formFactor = '';
+let slotSataAvailable = '';
+let slotNVMEAvailable = '';
+let slotRamAvailable = '';
+let slotRamUsed = '';
+let slotSataUsed = '';
+let slotNVMEUsed = '';
+let powerNeeded = 0;
+let socketRam = '';
+let socketCpu = '';
 let selectedElement;
 let selectedElementObject;
 let itemCategory;
-let tableHeader
+let tableHeader;
+let item;
 
 
 function toggleOverlay() {
@@ -29,182 +30,199 @@ function toggleOverlay() {
 }
 
 function updateSpecification() {
-    slotNVMEUsed=0;
-    slotRamUsed=0;
-    slotSataUsed=0;
-    powerNeeded=mobo.consumption+gpu.consumption+ram.consumption
-        +massStorage1.consumption+massStorage2.consumption+massStorage3.comsumption+cpu.consumption;
-    slotSataAvailable=mobo.amountSlotSata;
-    slotNVMEAvailable=mobo.amountSlotNvme;
-    slotRamAvailable=mobo.amountSlotRam;
-    slotRamUsed=ram.amountOfMemories;
+    slotNVMEUsed = 0;
+    slotRamUsed = 0;
+    slotSataUsed = 0;
+    powerNeeded = mobo.consumption + gpu.consumption + ram.consumption
+        + massStorage1.consumption + massStorage2.consumption + massStorage3.comsumption + cpu.consumption;
+    slotSataAvailable = mobo.amountSlotSata;
+    slotNVMEAvailable = mobo.amountSlotNvme;
+    slotRamAvailable = mobo.amountSlotRam;
+    slotRamUsed = ram.amountOfMemories;
 
-    socketRam=mobo.ramSocket;
-    socketCpu=mobo.cpuSocket;
-    formFactor=mobo.formFactor;
-    if(massStorage1!=null) {
-        if(massStorage1=="sata"){
-            slotSataAvailable-=1;
-        slotSataUsed+=1;}
-        else {slotNVMEAvailable-=1;
-        slotNVMEUsed+=1}
+    socketRam = mobo.ramSocket;
+    socketCpu = mobo.cpuSocket;
+    formFactor = mobo.formFactor;
+    if (massStorage1 != null) {
+        if (massStorage1 == "sata") {
+            slotSataAvailable -= 1;
+            slotSataUsed += 1;
+        } else {
+            slotNVMEAvailable -= 1;
+            slotNVMEUsed += 1
+        }
     }
-    if(massStorage2!=null) {
-        if(massStorage2=="sata"){
-            slotSataAvailable-=1;
-        slotSataUsed+=1;}
-        else {slotNVMEAvailable-=1;
-            slotNVMEUsed+=1}
+    if (massStorage2 != null) {
+        if (massStorage2 == "sata") {
+            slotSataAvailable -= 1;
+            slotSataUsed += 1;
+        } else {
+            slotNVMEAvailable -= 1;
+            slotNVMEUsed += 1
+        }
     }
-    if(massStorage3!=null) {
-        if(massStorage3=="sata"){
-            slotSataAvailable-=1;
-            slotSataUsed+=1;}
-        else {slotNVMEAvailable-=1;
-            slotNVMEUsed+=1}
+    if (massStorage3 != null) {
+        if (massStorage3 == "sata") {
+            slotSataAvailable -= 1;
+            slotSataUsed += 1;
+        } else {
+            slotNVMEAvailable -= 1;
+            slotNVMEUsed += 1
+        }
     }
 
 }
 
 
-
-function selectGPU(){
+function selectGPU() {
     toggleOverlay();
-    selectedElement="Gpus";
-
-    itemCategory="gpus";
-    tableHeader= "<tr><th>Product Name</th><th>Power Consumption</th><th>Price</th></tr>"
-    submitForm("Gpus");
+    selectedElement = "Gpus";
+    itemCategory = "gpus";
+    tableHeader = "<tr><th>Product Name</th><th>Power Consumption</th><th>Price</th></tr>";
+    item = "Gpus";
+    submitForm();
 
 }
-function selectCPU(){
-    toggleOverlay();
-    selectedElement="Cpus";
 
-    itemCategory="cpus";
+function selectCPU() {
+    toggleOverlay();
+    selectedElement = "Cpus";
+    itemCategory = "cpus";
     tableHeader = "<tr><th>Product Name</th><th>Socket</th><th>Integrated Gpu</th><th>Power Consumption</th><th>Price</th></tr>"
-    submitForm("Cpus");
+    item = "Cpus";
+    submitForm();
 }
-function selectMOBO(){
-    toggleOverlay();
-    selectedElement="Mobo";
 
-    itemCategory="motherboards";
+function selectMOBO() {
+    toggleOverlay();
+    selectedElement = "Mobo";
+
+    itemCategory = "motherboards";
     tableHeader = "<tr><th>Product Name</th><th>Form Factor</th><th>Ram Sockets</th><th>Ram Slots</th><th>NVME Slots</th><th>SATA Slots</th><th>Form Factor</th><th>Power Consumption</th><th>Price</th></tr>"
-    submitForm("Mobo");
+    item = "MotherBoards";
+    submitForm();
 
 }
 
-function selectRAM(){
+function selectRAM() {
     toggleOverlay();
-    selectedElement="Ram";
+    selectedElement = "Ram";
 
-    itemCategory="memories";
+    itemCategory = "memories";
     tableHeader = "<tr><th>Product Name</th><th>Socket</th><th>Number Of Sticks</th><th>Power Consumption</th><th>Price</th></tr>"
-    submitForm("Ram");
+    item = "Ram";
+
+    submitForm();
 
 }
-function selectMassStorage(number){
-    selectedElement="MassStorage";
 
-    itemCategory="memories";
+function selectMassStorage(number) {
+    toggleOverlay();
+    selectedElement = "MassStorage";
+
+    itemCategory = "memories";
     tableHeader = "<tr><th>Product Name</th><th>Power Consumption</th><th>Price</th></tr>"
-    submitForm("MassStorage",number);
+    item = "MassStorage";
+    submitForm(number);
 
 }
 
-function selectPcCase(){
+function selectPcCase() {
     toggleOverlay();
-    selectedElement="PcCase"
+    selectedElement = "PcCase"
 
-    itemCategory="cases";
+    itemCategory = "cases";
     tableHeader = "<tr><th>Product Name</th><th>Form Factor</th><th>Price</th></tr>"
-    submitForm("Cases");
+    item = "Cases";
+    submitForm();
 
 }
-function  selectPsu(){
+
+function selectPsu() {
     toggleOverlay();
-    selectedElement="Psu"
+    selectedElement = "Psu"
 
-    itemCategory="psus";
+    itemCategory = "psus";
     tableHeader = "<tr><th>Product Name</th><th>Power</th><th>Price</th></tr>"
-    submitForm("Psu");
+    item = "Psu";
+
+    submitForm();
 
 }
 
 
+function submitForm(number) {
 
-
-function submitForm(item,number) {
 
     let xhttp = new XMLHttpRequest();
     $('tr.removable').remove();
 
     let formData;
-    /*updateSpecification();*/
+    updateSpecification();
 
-    formData="name="+$("#productName").val();
-    let socketEmpty=true;
+    formData = "name=" + $("#productName").val().replaceAll(" ", "%20");
+    let socketEmpty = true;
 
 
-    if(selectedElement=="MassStorage"){
-        if($("#sata").prop("checked")) {
+    if (selectedElement == "MassStorage") {
+        if ($("#sata").prop("checked")) {
             formData += "&socket=sata&amountOfMemories=" + slotSataAvailable;
-            slotNVMEUsed-=1;
+            slotNVMEUsed -= 1;
+        } else {
+            formData += "&socket=nvme&amountOfMemories=" + slotNVMEAvailable;
+            slotSataUsed -= 1;
         }
-        else{ formData+="&socket=nvme&amountOfMemories="+slotNVMEAvailable;
-            slotSataUsed-=1;
-        }
-        formData+="&mType=true"
-        socketEmpty=false;
+        formData += "&mType=true"
+        socketEmpty = false;
     }
-    if($("#compatible").prop("checked")){
-        if(selectedElement=="Ram"){
-            socketEmpty=false;
-            formData+="&MEMsocket="+socketRam+"&amountOfMemories="+slotRamAvailable+"&mType=false";}
+    if ($("#compatible").prop("checked")) {
+        if (selectedElement == "Ram") {
+            socketEmpty = false;
+            formData += "&MEMsocket=" + socketRam + "&amountOfMemories=" + slotRamAvailable + "&mType=false";
+        }
 
-        formData+="&power="+powerNeeded+"&id=''&formFactor="+formFactor+"&CPUsocket="+socketCpu+"&RAMsocket="+socketRam+"" +
-            "&nRAMSocket='"+ram.amountOfMemories+"'"+"&nSATASockets='"+slotSataUsed+"'&nNVMESockets="+slotNVMEUsed+"&requestedItem="+itemCategory;
-    }
-    else
-        formData+="&requestedItem="+itemCategory+"&power=&id=&formFactor=&CPUsocket=&RAMsocket=" +
-            "&nRAMSocket="+"&nSATASockets=&nNVMESockets=&requestedItem=&MEMsocket=&amountOfMemories=&mType=false";
+        formData += "&power=" + powerNeeded + "&id=&formFactor=" + formFactor + "&CPUsocket=" + socketCpu + "&RAMsocket=" + socketRam + "" +
+            "&nRAMSocket=" + slotRamUsed + "" + "&nSATASockets=" + slotSataUsed + "&nNVMESockets=" + slotNVMEUsed + "&requestedItem=" + itemCategory;
+    } else
+        formData += "&requestedItem=" + itemCategory + "&power=&id=&formFactor=&CPUsocket=&RAMsocket=" +
+            "&nRAMSockets=" + "&nSATASockets=&nNVMESockets=&MEMsocket=&amountOfMemories=&mType=false&integratedGpu=";
 
     xhttp.onreadystatechange = function () {
 
         if (this.readyState == 4 && this.status == 200) {
+
             console.log(this.responseText);
 
             var results = JSON.parse(this.responseText);
-            arrayElements=results;
+            arrayElements = results;
 
             switch (item) {
                 case "Gpus":
-                  results.forEach(gpuTabler);
+                    results.forEach(gpuTabler);
                     break;
 
                 case "Cpus":
-                     results.forEach(cpuTabler);
+                    results.forEach(cpuTabler);
                     break;
 
                 case "Ram":
-                     results.forEach(memoryTabler,"Ram");
+                    results.forEach(memoryTabler, {type: "Ram"});
                     break;
 
                 case "MassStorage":
-                    results.forEach(memoryTabler,"MassStorage",number);
+                    results.forEach(memoryTabler, {type: "MassStorage", number: number});
                     break;
 
                 case "MotherBoards":
-                      results.forEach(moboTabler);
+                    results.forEach(moboTabler);
                     break;
 
                 case "Cases":
-                      results.forEach(pcCaseTabler);
+                    results.forEach(pcCaseTabler);
                     break;
 
-                case "Psus":
-                      results.forEach(psusTabler);
+                case "Psu":
+                    results.forEach(psusTabler);
                     break;
                 default:
                     $("#searchResult").html("Cannot visualize");
@@ -228,7 +246,7 @@ function gpuTabler(value) {
         "<td class='consumption'>" + value.consumption + "</td>" +
         "<td class='price'>" + value.price + "$</td>" +
         "<td><img src=-'" + value.imagePath + "'></td>" +
-        buttonAdder(value.id,"Gpu") +
+        buttonAdder(value.id, "Gpu") +
         "</tr>";
     $("#searchResult").append(row);
 }
@@ -245,12 +263,12 @@ function cpuTabler(value) {
         "<td class='price'>" + value.price + "$</td>" +
         "<td class='inStock'>" + value.stock + "</td>" +
         "<td><img src=-'" + value.imagePath + "'></td>" +
-        buttonAdder(value.id,"Cpu") +
+        buttonAdder(value.id, "Cpu") +
         "</tr>"
     $("#searchResult").append(row);
 }
 
-function memoryTabler(value,type,number) {
+function memoryTabler(value) {
     var row;
     row = "<tr class='removable'>" +
         "<td class='productName'>" + value.name + "</td>" +
@@ -258,7 +276,7 @@ function memoryTabler(value,type,number) {
         "<td class='consumption'>" + value.consumption + "</td>" +
         "<td class='price'>" + value.price + "$</td>" +
         "<td><img src=-'" + value.imagePath + "'></td>" +
-        buttonAdder(value.id,"type",number) +
+        buttonAdder(value.id, this.type, this.number) +
         "</tr>"
     $("#searchResult").append(row);
 }
@@ -276,7 +294,7 @@ function moboTabler(value) {
         "<td class='consumption'>" + value.consumption + "</td>" +
         "<td class='price'>" + value.price + "$</td>" +
         "<td><img src=-'" + value.imagePath + "'></td>" +
-        buttonAdder(value.id,"Mobo") +
+        buttonAdder(value.id, "Mobo") +
         "</tr>"
     $("#searchResult").append(row);
 }
@@ -288,7 +306,7 @@ function pcCaseTabler(value) {
         "<td class='formFactor'>" + value.formFactor + "</td>" +
         "<td class='price'>" + value.price + "$</td>" +
         "<td><img src=-'" + value.imagePath + "'></td>" +
-        buttonAdder(value.id,"pcCase") +
+        buttonAdder(value.id, "pcCase") +
         "</tr>"
     $("#searchResult").append(row);
 }
@@ -310,7 +328,7 @@ function buildTabler(value) {
 
     row += "<td class='buildType'>" + value.type + "</td>" +
         "<td class='maker'>" + value.maker + "</td>" +
-        buttonAdder(value.id,"pcCase") +
+        buttonAdder(value.id, "pcCase") +
         "</tr>"
     $("#searchResult").append(row);
 }
@@ -322,89 +340,124 @@ function psusTabler(value) {
         "<td class='power'>" + value.power + "</td>" +
         "<td class='price'>" + value.price + "$</td>" +
         "<td><img src=-'" + value.imagePath + "'></td>" +
-        buttonAdder(value.id,"Psu") +
+        buttonAdder(value.id, "Psu") +
         "</tr>"
     $("#searchResult").append(row);
 }
 
 
-function buttonAdder(id,type,number) {
+function buttonAdder(id, type, number) {
     let buttonForm;
     let action;
-    if(type!=null&&number!=null)
-         action="add"+type+number+"("+id+")";
-    else{
-        action="add"+type+"("+id+")";
+    if (type != null && number != null)
+        action = "add" + type + number + "(" + id + ")";
+    else {
+        action = "add" + type + "(" + id + ")";
     }
-    buttonForm = "<td><form id='" + id + "' onclick='"+action+"'>" +
+    buttonForm = "<td><form id='" + id + "' onclick='" + action + "'>" +
         "<h1 class='btn active'>Add</h1></form></td>";
     return buttonForm;
 }
 
 
-function addGpu(id){
-    let elem=arrayElements.find(elem=>elem.id==id);
-    gpu=elem;
-    selectedElementObject=elem;
-    html="<option value='id'>"+elem.name+"</option>"
-    $("#gpu").html()
+function addGpu(id) {
+
+    let elem = arrayElements.find(elem => elem.id == id);
+    gpu = elem;
+    selectedElementObject = elem;
+    $("#gpu").html("<option value='id'>" + elem.name + "</option>")
+    toggleOverlay();
+
 }
 
-function addCpu(id){
-    let elem=arrayElements.find(elem=>elem.id==id);
-    cpu=elem;
-    selectedElementObject=elem;
-    html="<option value='id'>"+elem.name+"</option>"
-    $("#cpu").html()
+function addCpu(id) {
+    let elem = arrayElements.find(elem => elem.id == id);
+    cpu = elem;
+    selectedElementObject = elem;
+    $("#cpu").html("<option value='id'>" + elem.name + "</option>")
+    toggleOverlay();
 }
 
-function addMobo(id){
-    let elem=arrayElements.find(elem=>elem.id==id);
-    mobo=elem;
-    selectedElementObject=elem;
-    html="<option value='id'>"+elem.name+"</option>"
-    $("#mobo").html()
-}
-function addpcCase(id){
-    let elem=arrayElements.find(elem=>elem.id==id);
-    pcCase=elem;
-    selectedElementObject=elem;
-    html="<option value='id'>"+elem.name+"</option>"
-    $("#pcCase").html()
-}
-function addPsu(id){
-    let elem=arrayElements.find(elem=>elem.id==id);
-    psu=elem;
-    selectedElementObject=elem;
-    html="<option value='id'>"+elem.name+"</option>"
-    $("#psu").html()
-}
-function addRam(id){
-    let elem=arrayElements.find(elem=>elem.id==id);
-    ram=elem;
-    selectedElementObject=elem;
-    html="<option value='id'>"+elem.name+"</option>"
-    $("#ram").html()
-}
-function addMassStorage1(id){
-    let elem=arrayElements.find(elem=>elem.id==id);
-    massStorage1=elem;
-    selectedElementObject=elem;
-    html="<option value='id'>"+elem.name+"</option>"
-    $("#massStorage1").html()
+function addMobo(id) {
+    let elem = arrayElements.find(elem => elem.id == id);
+    mobo = elem;
+    selectedElementObject = elem;
+    $("#mobo").html("<option value='id'>" + elem.name + "</option>")
+    toggleOverlay();
 }
 
-function addMassStorage2(id){
-    let elem=arrayElements.find(elem=>elem.id==id);
-    massStorage2=elem;
-    selectedElementObject=elem;
-    html="<option value='id'>"+elem.name+"</option>"
-    $("#massStorage2").html()
+function addpcCase(id) {
+    let elem = arrayElements.find(elem => elem.id == id);
+    pcCase = elem;
+    selectedElementObject = elem;
+    $("#pcCase").html("<option value='id'>" + elem.name + "</option>")
+    toggleOverlay();
 }
-function addMassStorage3(id){
-    let elem=arrayElements.find(elem=>elem.id==id);
-    massStorage3=elem;
-    selectedElementObject=elem;
-    html="<option value='id'>"+elem.name+"</option>";
-    $("#massStorage3").html();
+
+function addPsu(id) {
+    let elem = arrayElements.find(elem => elem.id == id);
+    psu = elem;
+    selectedElementObject = elem;
+    $("#psu").html("<option value='id'>" + elem.name + "</option>")
+    toggleOverlay();
+}
+
+function addRam(id) {
+    let elem = arrayElements.find(elem => elem.id == id);
+    ram = elem;
+    selectedElementObject = elem;
+    $("#ram").html("<option value='id'>" + elem.name + "</option>")
+    toggleOverlay();
+}
+
+function addMassStorage1(id) {
+    let elem = arrayElements.find(elem => elem.id == id);
+    massStorage1 = elem;
+    selectedElementObject = elem;
+    $("#massStorage1").html("<option value='id'>" + elem.name + "</option>");
+    toggleOverlay();
+}
+
+function addMassStorage2(id) {
+    let elem = arrayElements.find(elem => elem.id == id);
+    massStorage2 = elem;
+    selectedElementObject = elem;
+    $("#massStorage2").html("<option value='id'>" + elem.name + "</option>")
+    toggleOverlay();
+}
+
+function addMassStorage3(id) {
+    let elem = arrayElements.find(elem => elem.id == id);
+    massStorage3 = elem;
+    selectedElementObject = elem;
+    $("#massStorage3").html("<option value='id'>" + elem.name + "</option>")
+    toggleOverlay();
+}
+
+function saveBuild() {
+    let build = {
+        mobo: mobo.id,
+        gpu: gpu.id,
+        cpu: cpu.id,
+        pcCase: pcCase.id,
+        psu: psu.id,
+        type: "",
+        memories: [massStorage1.id, massStorage2.id, massStorage3.id]
+    }
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+        } else if (this.onreadystatechange() == 4 && this.status == 404) {
+            console.log("error");
+            $("#searchResult").append("Saving Error");
+        }
+    };
+
+    xhttp.open("POST", "/MYOPSite_war_exploded/itemsLister", true);
+    xhttp.setRequestHeader("Content-type", "plain/text");
+    let sentData = JSON.stringify(build);
+    console.log(sentData);
+    xhttp.send(sentData);
+
 }
