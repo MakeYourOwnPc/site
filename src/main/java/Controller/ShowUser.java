625211705ab4fbc9817e54a13fdfa07a1d44d2f6
alemarigliano16 <a.marigliano16@studenti.unisa.it>
2021-07-08 18:16:08 +0200

    pippo

---
+++ src/main/java/Controller/ShowUser.java
@@ -0,0 +1,21 @@
+package Controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet(name="showUser",urlPatterns = "/showUser")
+public class ShowUser extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(req.getSession().getAttribute("user")!=null){
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/MYOPSite_war_exploded/UserPages/userData.jsp");
+            dispatcher.forward(req,resp);
+        }
+        else
+            resp.setStatus(403);
+    }
+}
