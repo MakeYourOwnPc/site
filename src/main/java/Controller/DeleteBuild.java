4bc16a855f916a83336ab8d29d05a175de18d211
alemarigliano16 <a.marigliano16@studenti.unisa.it>
2021-07-09 20:09:49 +0200

    pippo

--- src/main/java/Controller/DeleteBuild.java
+++ src/main/java/Controller/DeleteBuild.java
@@ -9,14 +9,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 @WebServlet(name="deleteBuild",urlPatterns = "/deleteBuild")
 public class DeleteBuild extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = (User) req.getSession().getAttribute("user");
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("user");
         String id = req.getParameter("id");
+        int idBuild;
         if(user==null){
             resp.setStatus(403);
             return;
@@ -25,18 +28,23 @@ public class DeleteBuild extends HttpServlet {
             resp.setStatus(400);
             return;
         }
+        idBuild=Integer.parseInt(id);
         BuildDao buildDao = new BuildDao();
         Build build = null;
         try {
             if(!user.isAdmin()) {
-                build = buildDao.doRetrieveById(Integer.parseInt(id));
+                build = buildDao.doRetrieveById(idBuild);
                 if (!build.getMaker().equals(user.getEmail())) {
                     resp.setStatus(403);
                     return;
                 }
             }
-            if(buildDao.doDelete(Integer.parseInt(id)))
+            if(buildDao.doDelete(idBuild)) {
                 resp.setStatus(200);
+                int idSessionBuild = (int) session.getAttribute("id");
+                if(idBuild==idSessionBuild)
+                    session.removeAttribute("id");
+            }
             else
                 resp.setStatus(500);
         } catch (SQLException throwables) {
