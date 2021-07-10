504389d5dd1224d916933761535e8536cec1bbee
alemarigliano16 <a.marigliano16@studenti.unisa.it>
2021-07-10 18:17:58 +0200

    pippo

--- src/main/java/Controller/ShowCart.java
+++ src/main/java/Controller/ShowCart.java
@@ -6,12 +6,13 @@ import Model.User.User;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
-
+@WebServlet(name="showCart",urlPatterns = "/showCart")
 public class ShowCart extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
