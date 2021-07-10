7573e95ce70888c127c264f109042765d5ac20de
alemarigliano16 <a.marigliano16@studenti.unisa.it>
2021-07-10 13:43:15 +0200

    pippo

--- src/main/java/Controller/adminPage.java
+++ src/main/java/Controller/adminPage.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-@WebServlet(name="adminPage",urlPatterns = "/admin")
+
 public class adminPage extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
