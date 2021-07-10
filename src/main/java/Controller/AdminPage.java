ed8a0d3687174fb3bddd345a76a76294c9d8ace3
alemarigliano16 <a.marigliano16@studenti.unisa.it>
2021-07-11 00:02:34 +0200

    pippo

--- src/main/java/Controller/AdminPage.java
+++ src/main/java/Controller/AdminPage.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-public class adminPage extends HttpServlet {
+public class AdminPage extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
---
+++ src/main/java/Controller/InitServlet.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import Model.Country.Country;
+import Model.Country.CountryDao;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@WebServlet(value="/initServlet",loadOnStartup=0)
+public class InitServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        ServletContext servletContext = config.getServletContext();
+        CountryDao countryDao = new CountryDao();
+        try {
+            ArrayList<Country> countries = countryDao.doRetrieveAll();
+            servletContext.setAttribute("countries",countries);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
