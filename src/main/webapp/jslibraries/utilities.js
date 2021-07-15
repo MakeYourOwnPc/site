b5dca86622574112248cb4acb337364469d5300d
alemarigliano16 <a.marigliano16@studenti.unisa.it>
2021-07-15 09:07:56 +0200

    pippo

--- src/main/webapp/jslibraries/utilities.js
+++ src/main/webapp/jslibraries/utilities.js
@@ -77,7 +77,7 @@ function createToast(title,message){
     let toastHtml;
     toastHtml="<div id=\"toast\" class=\"toast-box\">\n" +
         "    <div class=\"toast-header\">" +
-        "<button onclick=\"closeToast()\" style=\"margin: 6px;\" class=\"btn-danger btn\">X</button>" +
+        "<button onclick=\"closeToast()\" style=\"padding: ;margin: 6px;\" class=\"btn-danger btn\">X</button>" +
         "<h3 id=\"toastTitle\">"+title+"</h3></div>" +
         "    <div class=\"toast-body\"><span id=\"toastMessage\">"+message+"</span></div>\n" +
         "</div>";
